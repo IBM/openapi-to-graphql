42d78351d795bf75c9a7c68ffd312b3eb94027c7
Erik Wittern <witternj@us.ibm.com>
2017-06-06 10:14:27 -0400

    added first tests to address #7

---
+++ test/test.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const request = require('request')
+const test = require('tape')
+const OasGraph = require('../index.js')
+const graphql = require('graphql').graphql
+
+/**
+ * These tests require the Example API to be running.
+ */
+
+let oas = require('./example_oas.json')
+OasGraph.createGraphQlSchema(oas)
+  .then(schema => {
+
+    /**
+     * Basic fetching of resources
+     */
+    test('Get user', t => {
+      let query = `{
+        user (username: "erik") {
+          name
+        }
+      }`
+      graphql(schema, query).then(result => {
+        t.ok(result.data.user.name === 'Erik Wittern', 'name correct')
+        t.end()
+      })
+    })
+
+    test('Get company', t => {
+      let query = `{
+        company (id: "ibm") {
+          legalForm
+        }
+      }`
+      graphql(schema, query).then(result => {
+        t.ok(result.data.company.legalForm === 'public', 'legalForm correct')
+        t.end()
+      })
+    })
+
+  })
+  .catch(err => {
+    console.log(err)
+  })
