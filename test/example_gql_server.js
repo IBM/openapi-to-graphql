8ab4bb926e872ee604172975c55f104c1657a242
Alan Cha <alan.cha@ibm.com>
2017-07-18 14:22:19 -0400

    Test code for Box API

--- test/example_gql_server.js
+++ test/example_gql_server.js
@@ -4,25 +4,29 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const app = express()
 const OasGraph = require('../index.js')
+const yamljs = require('yamljs')
+const fs = require('fs')
 
-let oas = require('./fixtures/example_oas.json')
+// let oas = require('./fixtures/example_oas.json')
 // let oas = require('./fixtures/github_oas.json')
 // let oas = require('./fixtures/instagram.json')
 // let oas = require('./fixtures/government_social_work_api.json')
 
+// requires Box API from API Guru
+let oas = yamljs.parse(fs.readFileSync('../tmp/APIs/box.com/content/2.0/swagger.yaml', 'utf8'))
+
 OasGraph.createGraphQlSchema(oas, {
   addSubOperations: true
-})
-  .then(schema => {
-    app.use('/graphql', graphqlHTTP({
-      schema: schema,
-      graphiql: true
-    }))
+}).then(schema => {
+  app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  }))
 
-    app.listen(3001, () => {
-      console.log('GraphQL accessible at: http://localhost:3001/graphql')
-    })
+  app.listen(3001, () => {
+    console.log('GraphQL accessible at: http://localhost:3001/graphql')
   })
-  .catch(err => {
-    console.log(err)
-  })
+})
+.catch(err => {
+  console.log(err)
+})
