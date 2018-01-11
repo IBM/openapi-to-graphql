d435ba1d33c41494c47477e7f319af8bb9b1333c
Erik Wittern <witternj@us.ibm.com>
2018-01-11 10:34:38 -0500

    fixed example GraphQL server

--- test/example_gql_server.js
+++ test/example_gql_server.js
@@ -15,7 +15,8 @@ let oas = require('./fixtures/example_oas.json')
 // // requires Box API from API Guru
 // let oas = yamljs.parse(fs.readFileSync('../tmp/APIs/box.com/content/2.0/swagger.yaml', 'utf8'))
 
-OasGraph.createGraphQlSchema(oas, {strict: true}).then(schema => {
+OasGraph.createGraphQlSchema(oas, {strict: true})
+.then(({schema}) => {
   app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true
