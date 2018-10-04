01b08632b6cbbb31493d282490fe35ceb16f4357
Mario Estrada <marioestradarosa@yahoo.com>
2018-10-04 08:59:51 -0600

    refactor(cli) add support for remote spec file
    Signed-off-by: Mario Estrada <marioestradarosa@yahoo.com>

--- cli.js
+++ cli.js
@@ -9,46 +9,79 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const OasGraph = require('./lib/index.js')
 const path = require('path')
+const request = require('request')
+const fs = require('fs')
 
 const app = express()
-const [,, ... args] = process.argv
 
-// Check for arguments
-if (args.length > 0) {
-  let filePath = args[0]
+if (process.argv.length <= 2) {
+  const usage = ' <OAS JSON file path or remote url> [port number]'
+  console.log('Usage: ' + __filename + usage)
+  process.exit(-1)
+}
 
-  // Convert path to absolute path
-  // Otherwise, would need to have separate cases for relative or absolute paths
-  filePath = path.resolve(filePath)
+let filePath = process.argv[2]
+let portNumber = process.argv[3] ? process.argv[3] : 3001
 
-  try {
-    let oas = require(filePath)
+// check if the file exists 
+if (fs.existsSync(path.resolve(filePath))) {
+  let oas = JSON.stringify(require(path.resolve(filePath)))
+  startGraphQLServer(oas, portNumber)
+    
+} else { // falls back to a remote location
+  if (filePath.match(/^https?/g)) {
+    getRemoteFileSpec(filePath).then(remoteContent=> {
+    startGraphQLServer(remoteContent, portNumber)
+  })
+  } else {
+    console.log(`OASGraph reading local file error. file ${filePath} does not exist.`)
+    } 
+}
 
-    // Create GraphQL interface
-    OasGraph.createGraphQlSchema(oas)
-      .then(({schema, report}) => {
-        console.log(JSON.stringify(report, null, 2))
-        app.use('/graphql', graphqlHTTP({
-          schema: schema,
-          graphiql: true
-        }))
+/**
+ * reads a remote file content using http protocol
+ * @param {string} url specifies a valid URL path including the port number
+ */
+function getRemoteFileSpec (uri) {
+  return new Promise((resolve, reject) => {
+    request({
+      uri,
+      json: true
+    }, (err, res, body) => {
+      if (err) {
+        reject(err)
+      } else if (res.statusCode !== 200) {
+        reject(new Error(`Error: ${JSON.stringify(body)}`))
+      } else {
+        resolve(body)
+      }
+    })
+  })
+}
 
-        app.listen(3001, () => {
-          console.log('GraphQL accessible at: http://localhost:3001/graphql')
-        })
+/**
+ * generates a GraphQL schema and starts the GraphQL server on the specified port 
+ * @param {Object} oas the OAS specification file
+ * @param {number} thePort the port number to listen on on this server
+ */
+function startGraphQLServer(oas, thePort) {
+  // Create GraphQL interface
+  OasGraph.createGraphQlSchema(oas) 
+     .then(({schema, report}) => {
+      console.log(JSON.stringify(report, null, 2))
+
+      // mounting graphql endpoint using the middleware express-graphql
+      app.use('/graphql', graphqlHTTP({
+        schema: schema,
+        graphiql: true
+      }))
+
+      // initiating the server on the port specified by user or the default one
+      app.listen(thePort, () => {
+        console.log(`GraphQL accessible at: http://localhost:${thePort}/graphql`)
       })
-      .catch(err => {
-        console.log(err)
-      })
-  } catch (error) {
-    if (error.code === 'MODULE_NOT_FOUND') {
-      console.log(`The file path "${filePath}" is invalid.`)
-    } else {
-      console.log(error)
-    }
-  }
-} else {
-  console.log(
-    'Usage: oasgraph <OAS JSON file path>'
-  )
+    })
+    .catch(err => {
+       console.log('OASGraph creation event error: ', err.message)
+     })
 }
