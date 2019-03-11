c934984732cad3e67558d702d9add8e119c95325
Alan Cha <Alan.cha1@ibm.com>
2019-03-11 13:23:35 -0400

    Fixed AnyAuth viewer
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- packages/oasgraph/test/example_gql_server.js
+++ packages/oasgraph/test/example_gql_server.js
@@ -24,7 +24,7 @@ let oas3 = require('./fixtures/example_oas3.json')
 // // requires Box API from API Guru
 // let oas = yamljs.parse(fs.readFileSync('../tmp/APIs/box.com/content/2.0/swagger.yaml', 'utf8'))
 
-OasGraph.createGraphQlSchema([oas, oas3])
+OasGraph.createGraphQlSchema(oas)
   .then(({ schema, report }) => {
     console.log(JSON.stringify(report, null, 2))
     app.use('/graphql', graphqlHTTP({
