10cea5ecf3e868225829d95971b3e707157cf8a3
Alan Cha <Alan.cha1@ibm.com>
2019-06-06 01:36:19 -0400

    Add preliminaryChecks method
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- packages/oasgraph/test/example_gql_server.js
+++ packages/oasgraph/test/example_gql_server.js
@@ -12,7 +12,7 @@ const OasGraph = require('../lib/index.js')
 
 let oas = require('./fixtures/example_oas.json')
 // let oas2 = require('./fixtures/example_oas2.json')
-// let oas3 = require('./fixtures/example_oas3.json')
+let oas3 = require('./fixtures/example_oas3.json')
 
 // let oas = require('./fixtures/github_oas.json')
 // let oas = require('./fixtures/instagram.json')
@@ -24,7 +24,7 @@ let oas = require('./fixtures/example_oas.json')
 // // requires Box API from API Guru
 // let oas = yamljs.parse(fs.readFileSync('../tmp/APIs/box.com/content/2.0/swagger.yaml', 'utf8'))
 
-OasGraph.createGraphQlSchema(oas, { provideErrorExtension: false, customResolvers: {
+OasGraph.createGraphQlSchema([oas, oas3], { provideErrorExtension: false, customResolvers: {
   'Example API': {
     '/users/{username}': {
       'get': () => {
