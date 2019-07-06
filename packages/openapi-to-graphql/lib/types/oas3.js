ba7722b09251d7c358aeefc013c97d96610d36c2
Jennie Ji <jennie.ji@hotmail.com>
2019-07-06 13:53:16 +0000

    Add prepublish script
    Signed-off-by: Jennie Ji <jennie.ji@shopee.com>

--- packages/openapi-to-graphql/lib/types/oas3.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Copyright IBM Corp. 2018. All Rights Reserved.
-// Node module: openapi-to-graphql
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=oas3.js.map
