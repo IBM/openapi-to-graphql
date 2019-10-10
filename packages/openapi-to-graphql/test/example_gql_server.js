01d464ec51d25b5f51a4b9f710956ae7c9d8cb25
Alan Cha <Alan.cha1@ibm.com>
2019-10-10 11:46:11 -0400

    Correction
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- packages/openapi-to-graphql/test/example_gql_server.js
+++ packages/openapi-to-graphql/test/example_gql_server.js
@@ -38,8 +38,8 @@ openapiToGraphql
       })
     )
 
-    app.listen(3001, () => {
-      console.log('GraphQL accessible at: http://localhost:3001/graphql')
+    app.listen(3000, () => {
+      console.log('GraphQL accessible at: http://localhost:3000/graphql')
     })
   })
   .catch(err => {
