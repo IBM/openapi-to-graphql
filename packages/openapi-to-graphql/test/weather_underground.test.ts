19161d2a5ea69a46740e71cc4611dfdec1aaa2a5
Alan Cha <Alan.cha1@ibm.com>
2020-01-07 15:24:05 -0500

    Clean up fixtures and tests
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

---
+++ packages/openapi-to-graphql/test/weather_underground.test.ts
@@ -0,0 +1,35 @@
+// Copyright IBM Corp. 2017,2018. All Rights Reserved.
+// Node module: openapi-to-graphql
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict'
+
+/* globals beforeAll, test, expect */
+
+import * as openAPIToGraphQL from '../lib/index'
+
+/**
+ * Set up the schema first
+ */
+const oas = require('./fixtures/weather_underground.json')
+
+let createdSchema
+beforeAll(() => {
+  return openAPIToGraphQL
+    .createGraphQlSchema(oas)
+    .then(({ schema, report }) => {
+      createdSchema = schema
+    })
+})
+
+test('All Weather Underground query endpoints present', () => {
+  let oasGetCount = 0
+  for (let path in oas.paths) {
+    for (let method in oas.paths[path]) {
+      if (method === 'get') oasGetCount++
+    }
+  }
+  const gqlTypes = Object.keys(createdSchema._typeMap.Query.getFields()).length
+  expect(gqlTypes).toEqual(oasGetCount)
+})
