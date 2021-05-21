503f31cb6db282c50ed53f0eb4bc6146534813db
Elias Meire <elias@meire.dev>
2021-05-21 14:26:43 +0200

    Add support for writeOnly
    Signed-off-by: Elias Meire <elias@meire.dev>

--- packages/openapi-to-graphql/src/types/oas3.ts
+++ packages/openapi-to-graphql/src/types/oas3.ts
@@ -32,6 +32,7 @@ export type SchemaObject = {
   oneOf?: (SchemaObject | ReferenceObject)[]
   not?: (SchemaObject | ReferenceObject)[]
   readOnly?: boolean
+  writeOnly?: boolean
 }
 
 export type ReferenceObject = {
